c9ed17f6107d58bbe6ba7af7607804631fa7d79c
yanisse ismaili <yanisseismaili@ynov.com>
2022-10-06 12:17:21 +0200

    nain

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -9,12 +9,3 @@ func main() {
 	}
 	z01.PrintRune('\n')
 }
-package main
-import "github.com/01-edu/z01"
-func main() {
-	for i := 97; i <= 122; i++ {
-		z01.PrintRune(rune(i))
-		
-	}
-	z01.PrintRune('\n')
-}
