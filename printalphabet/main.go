9862620cb29ab72a09ce8d443b184306f86c90d1
yanisse ismaili <yanisseismaili@ynov.com>
2022-10-06 12:09:20 +0200

    la

---
+++ printalphabet/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func main() {
+	for i := 97; i <= 122; i++ {
+		c := rune(i)
+		z01.PrintRune(c)
+	}
+	z01.PrintRune('\n')
+}
+package main
+import "github.com/01-edu/z01"
+func main() {
+	for i := 97; i <= 122; i++ {
+		z01.PrintRune(rune(i))
+		
+	}
+	z01.PrintRune('\n')
+}
