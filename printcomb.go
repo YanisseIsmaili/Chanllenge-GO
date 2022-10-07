2d91d1edee11e9b83ef1499bd68c4a1fcc0cc86b
yanisse ismaili <yanisseismaili@ynov.com>
2022-10-07 12:08:03 +0200

    naingi

--- printcomb.go
+++ printcomb.go
@@ -3,30 +3,30 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-    for i := 48; i <= 57; i++ {
-        a := rune(i)
+	for i := 48; i <= 57; i++ {
+		a := rune(i)
 
-        for j := 48; j <= 57; j++ {
-            b := rune(j)
+		for j := 48; j <= 57; j++ {
+			b := rune(j)
 
-            for k := 48; k <= 57; k++ {
-                c := rune(k)
+			for k := 48; k <= 57; k++ {
+				c := rune(k)
 
-                if i < j && j < k {
-                    z01.PrintRune(a)
-                    z01.PrintRune(b)
-                    z01.PrintRune(c)
-                    if i == 55 && j == 56 && k == 57 {
-                        z01.PrintRune(10)
-                    } else {
-                        z01.PrintRune(44)
-                        z01.PrintRune(32)
-                    }
-                }
-            }
-        }
-    }
-
+				if i < j && j < k {
+					z01.PrintRune(a)
+					z01.PrintRune(b)
+					z01.PrintRune(c)
+					if i == 55 && j == 56 && k == 57 {
+						z01.PrintRune(10)
+					} else {
+						z01.PrintRune(44)
+						z01.PrintRune(32)
+					}
+				}
+			}
+		}
+	}
+}
 
 /*
 i = 0	i+48 (valeur aski)
