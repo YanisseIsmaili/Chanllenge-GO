c10a3e79d749c0f879403ce7c335d8b45ad2ce5f
yanisse ismaili <yanisseismaili@ynov.com>
2022-10-07 12:27:34 +0200

    oj

--- TEST/main.go
+++ TEST/main.go
@@ -1,29 +1,7 @@
 package main
 
-import "github.com/01-edu/z01"
+import "piscine"
 
 func main() {
-	for i := 48; i <= 57; i++ {
-		a := rune(i)
-
-		for j := 48; j <= 57; j++ {
-			b := rune(j)
-
-			for k := 48; k <= 57; k++ {
-				c := rune(k)
-
-				if i < j && j < k {
-					z01.PrintRune(a)
-					z01.PrintRune(b)
-					z01.PrintRune(c)
-					if i == 55 && j == 56 && k == 57 {
-						z01.PrintRune(10)
-					} else {
-						z01.PrintRune(44)
-						z01.PrintRune(32)
-					}
-				}
-			}
-		}
-	}
+	piscine.PrintComb()
 }
