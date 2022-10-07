c1c5b2c18ad74116c4114cacd5d374bc6ae953b6
yanisse ismaili <yanisseismaili@ynov.com>
2022-10-07 17:28:06 +0200

    passe partout 12

--- TEST/main.go
+++ TEST/main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"github.com/01-edu/z01"
 	"piscine"
 )
 
 func main() {
-	z01.PrintRune(piscine.FirstRune("Hello!"))
-	z01.PrintRune(piscine.FirstRune("Salut!"))
-	z01.PrintRune(piscine.FirstRune("Ola!"))
-	z01.PrintRune('\n')
+	piscine.IsNegative(-12)
 }
