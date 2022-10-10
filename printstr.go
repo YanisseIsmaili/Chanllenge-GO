82cb6b4343915ce35453d0e5196c05e77de843a1
yanisse ismaili <yanisseismaili@ynov.com>
2022-10-10 18:47:06 +0200

    nain3.5

--- printstr.go
+++ printstr.go
@@ -5,7 +5,7 @@ import "github.com/01-edu/z01"
 func PrintStr(s string) {
 	s_rune := []rune(s)
 	size := len(s_rune)
-	for i := 0; i < size; i++ {
+	for i := 0; i < (size); i++ {
 		z01.PrintRune(s_rune[i])
 	}
 }
