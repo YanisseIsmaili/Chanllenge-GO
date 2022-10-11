529ccb208d16debb46e1321ec9e2c6a7e1a72cb9
yanisse ismaili <yanisseismaili@ynov.com>
2022-10-11 17:12:08 +0200

    nain3.9

---
+++ isupper.go
@@ -0,0 +1,14 @@
+package piscine
+
+func IsUpper(s string) bool {
+	runeBoard := []rune(s)
+	a := true
+	for _, r := range runeBoard {
+		if r >= 65 && r <= 90 {
+			a = true
+		} else {
+			a = false
+		}
+	}
+	return a
+}
