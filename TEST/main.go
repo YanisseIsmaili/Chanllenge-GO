e128e466703259252898d7e2c26b17a95a41e0c8
yanisse ismaili <yanisseismaili@ynov.com>
2022-10-11 18:28:42 +0200

    nain4.3

--- TEST/main.go
+++ TEST/main.go
@@ -6,6 +6,6 @@ import (
 )
 
 func main() {
-	fmt.Println(piscine.IsUpper("HELLO"))
-	fmt.Println(piscine.IsUpper("HELLO!"))
+	fmt.Println(piscine.IsNumeric("010203"))
+	fmt.Println(piscine.IsNumeric("01,02,03"))
 }
