1d56cfa36f76b21b4aa18392351e35050042f251
yanisse ismaili <yanisseismaili@ynov.com>
2022-10-11 18:26:57 +0200

    nain4.1

---
+++ isnumeric.go
@@ -0,0 +1,12 @@
+package piscine
+
+func IsNumeric(s string) bool {
+    t := []rune(s)
+    a := true
+    for _, r := range t {
+        if !(r >= 48 && r <= 57) {
+            a = false
+        }
+    }
+    return a
+}
