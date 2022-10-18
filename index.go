331474a311996bde312a66bb94e9e66c100f6e0a
yanisse ismaili <yanisseismaili@ynov.com>
2022-10-18 12:28:56 +0200

    zebi4h

--- index.go
+++ index.go
@@ -1,40 +1,40 @@
 package piscine
 
 func Index(s string, toFind string) int {
-    strtofind := NbretoFind(toFind) // prend comme valeur la str a rechercher
-    secondIndex := 0
-    for _, j := range toFind {
-        for i1, i2 := range s {
-            if i2 == j {
-                if strtofind == 1 {
-                    return i1
-                } else if strtofind > 1 { // si str a trouver > 1, crée une loop  pour vérifier
-                    for k := 0; k < strtofind; k++ {
-                        if s[i1+k] == toFind[k] { // si s = tofind, ajoute 1 au secondindex
-                            secondIndex++
-                        } else {
-                            return -1
-                        }
-                    }
-                    if secondIndex == strtofind { // si secondindex = au nbre de str a trouver, finis la boucle
-                        return i1
-                    }
-                } else { // sinon affiche -1
-                    return -1
-                }
-            }
-        }
-        if secondIndex <= 0 { // si pas de secondindex, affiche -1
-            return -1
-        }
-    }
-    return strtofind
+	strtofind := NbretoFind(toFind)
+	secondIndex := 0
+	for _, j := range toFind {
+		for i1, i2 := range s {
+			if i2 == j {
+				if strtofind == 1 {
+					return i1
+				} else if strtofind > 1 {
+					for k := 0; k < strtofind; k++ {
+						if s[i1+k] == toFind[k] {
+							secondIndex++
+						} else {
+							return -1
+						}
+					}
+					if secondIndex == strtofind {
+						return i1
+					}
+				} else {
+					return -1
+				}
+			}
+		}
+		if secondIndex <= 0 {
+			return -1
+		}
+	}
+	return strtofind
 }
 
-func NbretoFind(s string) int { // permet de savoir combien de str sont a trouver
-    compteur := 0
-    for i := range s {
-        compteur = i + 1
-    }
-    return compteur
+func NbretoFind(s string) int {
+	compteur := 0
+	for i := range s {
+		compteur = i + 1
+	}
+	return compteur
 }
