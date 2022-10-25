c4569bba58ccf8c4c4c67b8f16055bdec878c6ff
yanisse ismaili <yanisseismaili@ynov.com>
2022-10-25 13:50:02 +0200

    3mnmetjesuisnuldeoufla

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,11 +1,11 @@
 package piscine
 
-func RecursiveFactirial(nb int) int {
+func RecursiveFactorial(nb int) int {
 	a := 1
 	if nb == 1 || nb == 0 {
 		return 1
 	} else if nb > 1 && nb <= 25 {
-		nb *= RecursiveFactirial(nb - 1)
+		nb *= RecursiveFactorial(nb - 1)
 	} else {
 		return 0
 	}
